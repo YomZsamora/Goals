722cf2641a737109edc65708d50240d2dbd36171
yomz <samora.y@adzumi.co.ke>
2019-01-10 15:35:30 +0300

    Auto-generated commit

--- src/app/bold-font.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BoldFontDirective } from './bold-font.directive';
-
-describe('BoldFontDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BoldFontDirective();
-    expect(directive).toBeTruthy();
-  });
-});
